11d3de98c28ea0191b8b3f59f8c3e958db35430b
Mikita Pilets <mykytapilec@gmail.com>
2026-07-18 00:27:20 +0200

    feat: include exercises in workout responses

--- src/types/workout.types.ts
+++ src/types/workout.types.ts
@@ -1,7 +1,20 @@
+export interface Exercise {
+  id: string;
+  name: string;
+  sets: number;
+  reps: number;
+  weight: number | null;
+  createdAt: Date;
+  updatedAt: Date;
+  workoutId: string;
+}
+
 export interface Workout {
   id: string;
   name: string;
   createdAt: Date;
+  updatedAt: Date;
+  exercises?: Exercise[];
 }
 
 export interface CreateWorkoutInput {
